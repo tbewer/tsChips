2d1874185ae29952a2db463f66b579de13e45cf2
Ben DeVries <devries.br@gmail.com>
2014-09-04 16:50:05 +0200

    printing scene names to console for now...

--- R/subsetScenesPlot.R
+++ R/subsetScenesPlot.R
@@ -1,55 +1,33 @@
-#' @title Plot automatically subsetted scenes
-#' 
-#' @description plot scenes selected using \code{subsetScenes}. These can either by single layer or multilayer rasters (or a combination of the two), in which case this function will plot accordingly.
-#' 
-#' @param ... Arguments to be passed to \code{\link{subsetScenes}}
-#' @param bands Numeric. Optional: see \code{details}. 
-#' 
-#' @details If \code{subsetScenes} returns a multi-layered raster with \code{nlayers > 3}, \code{bands} determines which bands should be plotted (and in which order, according to RGB compositing). If ignored, the first 3 bands will be plotted in that order. If there are 2 layers, the first will be plotted as a single layer. 
-#'
-#' @return \code{NULL}, with the side effect of plotting outcome of \code{subsetScenes}
-#' 
-#' @author Ben DeVries
-#' 
-#' @import raster
-#' @import sp
-#' @export
-#' 
-
-subsetScenesPlot <- function(..., bands = NULL, stretch = NULL, textcol = "white") {
-  
+subsetScenesPlot <- function (..., bands = NULL, stretch = NULL, textcol = "white") 
+{
   op <- par(mfrow = c(1, 1))
   scenes <- subsetScenes(...)
+  fl <- sapply(scenes, FUN = function(x) names(x)[1])
   
-  # get scene filenames
-  ## TODO: replace this chunk with more elegant regex!
-  fl <- sapply(scenes, FUN=function(x) x@file@name)
-  pos <- sapply(fl, FUN=function(x) gregexpr("/", x))
-  n <- sapply(pos, FUN=function(x) x[length(x)] + 1)
-  newfl <- substr(fl, n, nchar(str))
-  
-  for(i in 1:length(scenes)) {
-    if(nlayers(scenes[[i]]) >= 3){
-      if(is.null(bands)) {
+  for (i in 1:length(scenes)) {
+    print(fl[i])
+    if (nlayers(scenes[[i]]) >= 3) {
+      if (is.null(bands)) {
         plotRGB(scenes[[i]], 1, 2, 3, stretch = stretch)
-      } else {
-        plotRGB(scenes[[i]], bands[1], bands[2], bands[3], stretch = stretch)
       }
-      text(x = (xmin(extent(scenes[[i]])) + xmax(extent(scenes[[i]])))/2, y = ymin(extent(scenes[[i]])) + 2*res(scenes[[i]])[1], labels = newfl[i], col = textcol)
-    } else if(nlayers(scenes[[i]]) == 2){
-      plot(raster(scenes[[i]], 1))
-    } else {
-      plot(scenes[[i]])
+      else {
+        plotRGB(scenes[[i]], bands[1], bands[2], bands[3], 
+                stretch = stretch)
+      }
+      e <- extent(scenes[[i]])
+      text(x = xmin(e) + xmax(e)/2, y = ymin(e) + 100*res(scenes[[i]])[1], labels = fl[i], col = textcol)
     }
-    
-    # function to add spatial data (if present)
+    else if (nlayers(scenes[[i]]) == 2) {
+      plot(raster(scenes[[i]], 1), main = fl[i])
+    }
+    else {
+      plot(scenes[[i]], main = fl[i])
+    }
     x <- list(...)[[1]]
-    if(class(x) %in% c("SpatialPolygons", "SpatialPolygonsDataFrame")){
-      plot(x, add=TRUE)
+    if (class(x) %in% c("SpatialPolygons", "SpatialPolygonsDataFrame")) {
+      plot(x, add = TRUE)
     }
-    
     readline("Press any key to continue to next screen: \n")
     par(op)
   }
-  
 }
